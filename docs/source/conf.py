ec8964c5e7d2ddefd9ccc29377c9b4955a0120a6
Abdullah Al Amin <nirjhor18@gmail.com>
2025-01-09 22:33:11 -0500

    Fixed main.py documentation issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../fame'))  # Path to your src folder
-
+sys.path.insert(0, os.path.abspath('../../fame'))  # Path to src folder
+sys.path.insert(0, os.path.abspath('../..'))  # Path to root
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
